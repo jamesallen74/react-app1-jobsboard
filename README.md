d260c259f55c9797cbbd787ad09501b2667f96ae
James Allen <jamesallen74@gmail.com>
2024-05-12 12:34:15 -0400

    add job functionality

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
+LAST SPOT
+https://www.youtube.com/watch?v=LDB4uaJ87e0?t=2h21m52s
+2:41:44  Delete
+
+**To get it started**
 1. clone repo to loca dev env
-1. open new command line instance
+1. open new terminal window
 1. run 'npm install'
 1. run 'npm run server' to start the api on port 8000 using json-server
-1. open new command line instance
-1. run 'npm run dev' to start the app on port 3000 using vite
+1. open new terminal window
+1. run 'npm run dev' to start the app on port 3000 using vite dev tools
 
+**Pre-requisite**
+Node (with NPM) installed
 
-//// LAST SPOT
-// https://www.youtube.com/watch?v=LDB4uaJ87e0
-// 2:12:25  JSON Server
+**Commands ran for project setup, already ran**
+`npm create vite@latest jobsboard`
+
+`npm i -D json-server`
+`npm i react-dom`
+`npm i react-icons`
+`npm i react-router-dom`
+`npm i react-spinners`
+`npm i react-toastify`
+
+**For Tailwind**
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+added tailwind.config.js and all of its content
+
+put these lines in index.css
+`@tailwind base;`
+`@tailwind components;`
+`@tailwind utilities;`
--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,35 @@ from "react-router-dom";
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
-import AddJob from './pages/AddJob';
-import JobPage from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
 import NotFound404 from './pages/NotFound404';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage/>} />
-    <Route path='/jobs' element={<JobsPage/>} />
-    <Route path='/jobs/:id' element={<JobPage/>} />
-    <Route path='/add-job' element={<AddJob/>} />
-    <Route path='*' element={<NotFound404/>} />
-  </Route>
-));
-
 const App = () => {
+
+  const addJob = async (newJob) => {
+    // console.log(newJob);
+    const res = await fetch('api/jobs', {
+      method : 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  }
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage/>} />
+        <Route path='/jobs' element={<JobsPage/>} />
+        <Route path='/jobs/:id' element={<JobPage/>} loader={jobLoader} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+        <Route path='*' element={<NotFound404/>} />
+    </Route>
+  ));
+
   return <RouterProvider router={router} />;
 };
 
